2099a82b9dce51ce9bf6f3e6d26d0c4a088d5416
KANG77556 <204034@gmail.com>
2026-08-08 03:29:01 +0900

    fix: support companion on Wear OS 4

--- companion/build.gradle.kts
+++ companion/build.gradle.kts
@@ -4,10 +4,10 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.kang77556.schoolwatch.companion"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
