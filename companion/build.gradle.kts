0731676aceae8af817761ecdc69120e2240a1758
KANG77556 <204034@gmail.com>
2026-08-08 00:13:37 +0900

    feat: add school companion module

---
+++ companion/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins { alias(libs.plugins.android.application) }
+android {
+    namespace = "com.kang77556.schoolwatch.companion"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.kang77556.schoolwatch.companion"
+        minSdk = 34
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+dependencies {
+    implementation(libs.wear.watchface.complications.data.source)
+}
