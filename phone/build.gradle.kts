c2e583a6a69f593ae83835f64c3064f83aeab0d0
KANG77556 <204034@gmail.com>
2026-08-09 08:05:09 +0900

    feat: add Wear OS Data Layer to phone companion

--- phone/build.gradle.kts
+++ phone/build.gradle.kts
@@ -3,7 +3,7 @@ android {
     namespace = "com.kang77556.schoolwatch.phone"
     compileSdk = 36
     defaultConfig {
-        applicationId = "com.kang77556.schoolwatch.phone"
+        applicationId = "com.kang77556.schoolwatch"
         minSdk = 33
         targetSdk = 36
         versionCode = 1
@@ -15,5 +15,6 @@ android {
     }
 }
 dependencies {
+    implementation("com.google.android.gms:play-services-wearable:19.0.0")
     testImplementation("junit:junit:4.13.2")
 }
