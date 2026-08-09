0fce8486206e74764711b08a33978bf5b578394c
KANG77556 <204034@gmail.com>
2026-08-09 20:33:31 +0900

    fix: add safe JSON picker contract

---
+++ phone/src/main/java/com/kang77556/schoolwatch/phone/JsonPickerContract.kt
@@ -0,0 +1,9 @@
+package com.kang77556.schoolwatch.phone
+
+object JsonPickerContract {
+    fun mimeTypes(): Array<String> = arrayOf(
+        "application/json",
+        "text/plain",
+        "application/octet-stream",
+    )
+}
